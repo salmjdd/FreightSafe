2f25b6dc712172b87b719bf399576f6830af95a2
sallyvalley <salmanmajid1975@gmail.com>
2023-10-25 15:47:04 -0400

    Added drive log methods

--- src/main/java/com/example/freightsafe/HelloApplication.java
+++ src/main/java/com/example/freightsafe/HelloApplication.java
@@ -18,7 +18,7 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        EquipmentInspection day1 = new EquipmentInspection();
+       EquipmentInspection day1 = new EquipmentInspection();
         day1.promptUserInput();
 
 
@@ -27,6 +27,19 @@ public class HelloApplication extends Application {
         } else {
             System.out.println("Inspection passed, drive safe");
         }
+        //if inspection is passed continue to log hours window
+        DriveLog drive = new DriveLog();
+        //clocking in takes current time and stores it as a variable
+        drive.clockIn();
+        System.out.println(drive.getClockedIn());
+        //clocking out takes the current time and stores it as a variable
+        drive.clockOut();
+        System.out.println(drive.getClockedOut());
+        //hours driven is the difference between clock in and clock out time converted into hours
+        System.out.println(drive.hoursDriven());
+
+        //to-do: perform algorithm to ensure driver hours are in compliance
+
 
 
 

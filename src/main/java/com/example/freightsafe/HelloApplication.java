b86e4e5bb549d826e0d0348efe591b4b0e5fa154
sallyvalley <salmanmajid1975@gmail.com>
2023-12-03 15:52:10 -0500

    Added accdb and login page

--- src/main/java/com/example/freightsafe/HelloApplication.java
+++ src/main/java/com/example/freightsafe/HelloApplication.java
@@ -6,43 +6,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
-       EquipmentInspection day1 = new EquipmentInspection();
-        day1.promptUserInput();
-
-
-        if(day1.checkInspectionReport() == false){
-            System.out.println("Inspection failed, please take action");
-        } else {
-            System.out.println("Inspection passed, drive safe");
-        }
-        //if inspection is passed continue to log hours window
-        DriveLog drive = new DriveLog();
-        //clocking in takes current time and stores it as a variable
-        drive.clockIn();
-        System.out.println(drive.getClockedIn());
-        //clocking out takes the current time and stores it as a variable
-        drive.clockOut();
-        System.out.println(drive.getClockedOut());
-        //hours driven is the difference between clock in and clock out time converted into hours
-        System.out.println(drive.hoursDriven());
-
-        //to-do: perform algorithm to ensure driver hours are in compliance
-
-
-
-
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
+        launch();
     }
 
 }
--- src/main/java/com/example/freightsafe/HelloController.java
+++ src/main/java/com/example/freightsafe/HelloController.java
@@ -1,14 +1,64 @@
 package com.example.freightsafe;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Hashtable;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    public TextField userText;
+    @FXML
+    public TextField passwordText;
+    public DataLoader test1 = new DataLoader();
+
+
+    public void initialize() throws SQLException, ClassNotFoundException, IOException {
+        test1.connectDB();
+        test1.loadData();
+
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void loginHandler(ActionEvent event) throws SQLException, ClassNotFoundException, IOException {
+        System.out.println("test");
+        Hashtable driverHash = test1.getDriverHash();
+        Hashtable agentHash = test1.getAgentHash();
+        String user = "", pass = "", enteredpass = "";
+        int numattempts = 0;
+        boolean loginValid = false;
+        String userID = "";
+
+        System.out.println("Welcome to FreightSafe");
+
+        do {
+            System.out.print("Enter your userID: ");
+            user = userText.getText();
+
+            if ((!driverHash.containsKey(user) && (!agentHash.containsKey(user)))){
+                System.out.println("Username is Invalid!");
+            } else {
+                System.out.print("Enter your password: ");
+                enteredpass = passwordText.getText();
+                pass = (String)driverHash.get(user);
+                if (!enteredpass.equals(pass)) {
+                    System.out.println("Password is Invalid!");
+                } else {
+                    loginValid = true;
+                    userID = user;
+                }
+            }
+            numattempts++;
+        } while ((numattempts < 3) && (!loginValid));
+        if (!loginValid) {
+            System.out.println("Error: Number of attempts exceeded, program terminated!");
+            System.exit(100);
+        }
+        userID = user;
+        System.out.println("Login success!");
     }
 }

2b13bf18271b1b11264b7b9dad42250b0d2beaf9
catamv3 <catamv3@farmingdale.edu>
2023-12-03 16:07:11 -0500

    gui for driver output

--- src/main/java/com/example/freightsafe/DriverAvailabilityController.java
+++ src/main/java/com/example/freightsafe/DriverAvailabilityController.java
@@ -1,4 +1,36 @@
 package com.example.freightsafe;
 
-public class DriverAvailabilityController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DriverAvailabilityController implements Initializable {
+
+    @FXML
+    private Label nameLabel,licenseNumLabel,phoneLabel,hasCDLLabel,cdlClassLabel,under21Label,hoursDrivenLLabel, isAvailableLabel,hasTakenBreak;
+
+    @FXML
+    private TableView<Driver> tv;
+
+    @FXML
+    private TableColumn<Driver, String> driverTVname;
+    @FXML
+    private TableColumn<Driver, Boolean> driverTVcdl, driverTVavailability;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        driverTVname.setCellValueFactory(new PropertyValueFactory<>("name"));
+        driverTVcdl.setCellValueFactory(new PropertyValueFactory<>("hasCDL"));
+        driverTVavailability.setCellValueFactory(new PropertyValueFactory<>("isAvailable"));
+
+
+
+    }
 }

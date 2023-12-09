8a3f480d573d7b2e3dc3bd2cf8cd364b776d48c7
catamv3 <catamv3@farmingdale.edu>
2023-12-09 15:02:26 -0500

    added text fields to truck avail view

--- src/main/java/com/example/freightsafe/AvailabilityViewController.java
+++ src/main/java/com/example/freightsafe/AvailabilityViewController.java
@@ -5,10 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
@@ -29,7 +26,10 @@ public class AvailabilityViewController implements Initializable {
     private TableColumn<Object, Boolean> tvAvailability;
 
     @FXML
-    private Button returnButton;
+    private Button returnButton, addButton;
+
+    @FXML
+    private TextField truckNumberTF, mileageTF, availabilityTF;
 
     @FXML
     private TableView<Object> tv;
